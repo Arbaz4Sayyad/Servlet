c202648fef5d9496778eef856afe54f5c4bf132e
arbaz4sayyad <arbaz4sayyad@gmail.com>
2024-04-27 22:57:33 +0530

    Html to Servlet Communication using Hyperlink

---
+++ WishApp/src/main/java/com/nt/servlet/WishMessageServlet.java
@@ -0,0 +1,46 @@
+package com.nt.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WishMessageServlet extends HttpServlet {
+	@Override
+	public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		
+		//get PrintWriter	
+		PrintWriter pw = res.getWriter();
+		
+		//set content type
+		res.setContentType("text/html");
+		
+		//write b.logic to generate the wish message
+		LocalDateTime ldt = LocalDateTime.now(); //gives current date and time
+		
+		int hour = ldt.getHour();//gives current hour of the day
+		pw.println("<h1 style='color:red; text-align:center'>"+ldt.toString()+"</h1>");
+		if(hour<12)
+			pw.println("<h1 style='color:orange; text-align:center'>Good Morning</h1>");
+		
+		else if(hour<16)
+			pw.println("<h1 style='color:green; text-align:center'>Good Afternoon</h1>");
+		
+		else if(hour<20)
+			pw.println("<h1 style='color:pink; text-align:center'>Good Evening</h1>");
+		
+		else
+			pw.println("<h1 style='color:red; text-align:center'>Good Night</h1>");
+		
+		//add home hyperlink
+		pw.println("<br><a href='http://localhost:3030/WishApp/page.html'>home</a>");
+		//close the stream
+		pw.close();
+		
+	}
+
+}
